64f675e761e5848eb0bae4accbe79b3f288d639d
IgorRos <lock_books@bk.ru>
2024-09-26 10:11:51 +0400

    article 3 final without comments

--- pygame3.py
+++ pygame3.py
@@ -15,58 +15,91 @@ screen = pygame.display.set_mode((width, height))
 fps = 60
 # создаём объект таймера
 clock = pygame.time.Clock()
-
-# создаём объекты шрифта: в скобках указываем шрифт и размер через запятую
-text_font = pygame.font.Font('prstartk.ttf', 15)
-text_font_collide = pygame.font.Font('prstartk.ttf', 50)
-
-# создаём новую поверхность земли отдельным объектом surface
-# в финальном коде не используется
-# 1 - задаём размеры:
-width_ts = 800
-height_ts = 200
-# 2 - создаём  поверхность по  размерам
-test_surface = pygame.Surface((width_ts, height_ts))
-# 3 - добавляем цвет
-test_surface.fill('Brown')
+# НОВОЕ!!! начальная точка отсчёта времени
+start_time = 0
+final_score = 0
 
 # загружаем в переменные картинки из папки с нашим файлом
+back_main_screen = pygame.image.load('code_game_back.jpg').convert()
 back = pygame.image.load('code_game_back_floor.jpg').convert()
 hero = pygame.image.load('detective.png').convert_alpha()
 pot = pygame.image.load('teapot.png').convert_alpha()
 candle = pygame.image.load('candlestick.png').convert_alpha()
 box = pygame.image.load('wooden_box.png').convert_alpha()
 
-# объявляем переменные с начальными координатами для всех анимаций
-hero_x_pos = 75
-hero_y_pos = 180
-candle_x_pos = 900
-candle_y_pos = 70
-box_x_pos = 900
-box_y_pos = 200
-pot_x_pos = 900
-pot_y_pos = 345
-
-# помещаем изображения в рамки прямоугольника
-# в скобках задаём точку привязки и координаты для неё
-hero_rect = hero.get_rect(center=(hero_x_pos, hero_y_pos))
-pot_rect = pot.get_rect(center=(pot_x_pos, pot_y_pos))
-candle_rect = candle.get_rect(center=(candle_x_pos, candle_y_pos))
-box_rect = box.get_rect(center=(box_x_pos, box_y_pos))
-
-# создаём сигнальные переменные
-pot_flag = False
-box_flag = False
-
+# !!! НОВОЕ!!! ещё одна настройка для новой игры
+# создаём объекты шрифта: в скобках указываем шрифт и размер через запятую
+# !!! НОВОЕ новый текст
 # создаём объект текста: в скобках указываем текст, сглаживание и цвет
+text_font = pygame.font.Font('prstartk.ttf', 15)
 text_surface = text_font.render('Detective CODE Game', False, 'White')
+text_name_rect = text_surface.get_rect(center=(400, 30))
+
+text_font_collide = pygame.font.Font('prstartk.ttf', 50)
 text_collide = text_font_collide.render('CoLLiDE!!', False, 'Red')
+text_collide_rect = text_collide.get_rect(center=(400, 200))
+
+text_font_new_game = pygame.font.Font('prstartk.ttf', 20)
+text_newgame_str1 = text_font_new_game.render('If you want to start,', False, 'Green')
+text_newgame_rect1 = text_newgame_str1.get_rect(center=(400, 325))
+text_newgame_str2 = text_font_new_game.render('press space', False, 'Green')
+text_newgame_rect2 = text_newgame_str2.get_rect(center=(400, 350))
+
+text_font_score = pygame.font.Font('prstartk.ttf', 15)
+
+text_ts_font = pygame.font.Font('prstartk.ttf', 20)
+# text_ts_text = text_ts_font.render('Total score: {xxx}', False, 'White')
+# text_ts_rect = text_ts_text.get_rect(center=(400, 250))
 
 # даём название окну игры
 pygame.display.set_caption('Detective CODE Game')
 
 # объявляем переменную-флаг для цикла игры
-game = True
+game = False
+
+
+# НОВОЕ!! подсчёт очков
+def display_score():
+    current_time = pygame.time.get_ticks() - start_time
+    score_surface = text_font_score.render(f'{current_time}', False, 'Purple')
+    score_rect = score_surface.get_rect(bottomright=(795, 395))
+    screen.blit(score_surface, score_rect)
+
+    return score_rect
+
+
+# Функция для сброса начальных параметров
+def reset_game():
+    global hero_rect, pot_rect, candle_rect, box_rect, pot_flag, box_flag, game
+
+    # объявляем начальные координаты для всех объектов
+    hero_x_pos = 75
+    hero_y_pos = 180
+    candle_x_pos = 900
+    candle_y_pos = 70
+    box_x_pos = 900
+    box_y_pos = 200
+    pot_x_pos = 900
+    pot_y_pos = 345
+
+    # создаём рамки прямоугольников
+    hero_rect = hero.get_rect(center=(hero_x_pos, hero_y_pos))
+    pot_rect = pot.get_rect(center=(pot_x_pos, pot_y_pos))
+    candle_rect = candle.get_rect(center=(candle_x_pos, candle_y_pos))
+    box_rect = box.get_rect(center=(box_x_pos, box_y_pos))
+
+    # сбрасываем флаги и состояние игры
+    pot_flag = False
+    box_flag = False
+
+    # сбрасываем финальный счет
+    final_score = 0
+
+    # game = True
+
+
+# Инициализация игры перед началом
+reset_game()
 
 # запускаем бесконечный цикл
 while True:
@@ -79,41 +112,26 @@ while True:
             # добавляем корректное завершение работы
             exit()
 
-        # перезапуск
+        # !!! НОВОЕ !!! перезапуск
         if not game and event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-            # заново объявляем переменные с начальными координатами для всех анимаций
-            hero_x_pos = 75
-            hero_y_pos = 180
-            candle_x_pos = 900
-            candle_y_pos = 70
-            box_x_pos = 900
-            box_y_pos = 200
-            pot_x_pos = 900
-            pot_y_pos = 345
-
-            # заново помещаем изображения в рамки прямоугольника
-            # в скобках задаём точку привязки и координаты для неё
-            hero_rect = hero.get_rect(center=(hero_x_pos, hero_y_pos))
-            pot_rect = pot.get_rect(center=(pot_x_pos, pot_y_pos))
-            candle_rect = candle.get_rect(center=(candle_x_pos, candle_y_pos))
-            box_rect = box.get_rect(center=(box_x_pos, box_y_pos))
-
-            # заново создаём сигнальные переменные
-            pot_flag = False
-            box_flag = False
-
+            reset_game()
             game = True
+            start_time = pygame.time.get_ticks()
 
     if game:
         # получаем список всех нажатых клавиш
         keys = pygame.key.get_pressed()
 
-        # если нажата клавиша вверх, двигаем игрока вверх
+        # если нажата клавиша вверх, двигаем игрока вверх !!! НОВОЕ!!! добавили проаерку границ и плавное передвижение
         if keys[pygame.K_UP]:
             hero_rect.top -= 5
+            if hero_rect.top <= 0:
+                hero_rect.top = 0
         # если нажата клавиша вниз, двигаем игрока вниз
         if keys[pygame.K_DOWN]:
             hero_rect.top += 5
+            if hero_rect.bottom >= 400:
+                hero_rect.bottom = 400
 
     if game:
         # размещаем все поверхности на нашем экране
@@ -122,7 +140,7 @@ while True:
         screen.blit(candle, candle_rect)
         screen.blit(box, box_rect)
         screen.blit(pot, pot_rect)
-        back.blit(text_surface, (250, 15))
+        back.blit(text_surface, text_name_rect)
 
         # запускаем движение всех предметов
         candle_rect.left -= 4
@@ -153,14 +171,27 @@ while True:
         if (hero_rect.colliderect(candle_rect) or
             hero_rect.colliderect(pot_rect) or
             hero_rect.colliderect(box_rect)):
-            screen.blit(text_collide, (200, 165))
+            screen.blit(text_collide, text_collide_rect)
+
+            # Вычисляем финальное время и сохраняем его
+            final_score = (pygame.time.get_ticks() - start_time) // 1000
+            text_ts_text = text_ts_font.render(f'Total time: {final_score} sec', False, 'White')
+            text_ts_rect = text_ts_text.get_rect(center=(400, 250))
+            screen.blit(text_ts_text, text_ts_rect)
+
             # это новый блок
             pygame.display.flip()
             time.sleep(2)
             game = False
 
+        display_score()
+
     else:
-        screen.fill('Purple')
+        screen.blit(back_main_screen, (0, 0))
+        pygame.draw.rect(back_main_screen, 'Black', (100, 300, 600, 80))
+
+        screen.blit(text_newgame_str1, text_newgame_rect1)
+        screen.blit(text_newgame_str2, text_newgame_rect2)
 
     # обновляем экран игры
     pygame.display.update()
